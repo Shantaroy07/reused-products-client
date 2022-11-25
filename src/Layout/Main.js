547cf279c85d59e3a7ff206817da458f46ba4cae
Shantaroy07 <shantaroy148@gmail.com>
2022-11-25 15:43:19 +0600

    navbar-added

---
+++ src/Layout/Main.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+
+
+        </div>
+    );
+};
+
+export default Main;
