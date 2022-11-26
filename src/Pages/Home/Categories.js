7c2fd96041ba90fd1b0e3e73847f5b5fe21f462b
Shantaroy07 <shantaroy148@gmail.com>
2022-11-26 14:33:19 +0600

    category added

---
+++ src/Pages/Home/Categories.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/home')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
+    return (
+        <div className='my-5 container mx-auto' >
+            <h1 className='text-2xl font-semibold text-blue-600 my-3'>Books Category</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3'>
+                {
+                    categories.map(category =>
+
+                        <div className="card card-compact w-96 bg-gray-300 shadow-xl">
+                            <figure><img className='h-48' src={category.img} alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title text-2xl ">{category.category_name}</h2>
+
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">See Books</button>
+                                </div>
+                            </div>
+                        </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import Categories from './Categories';
 import Info from './Info';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Categories></Categories>
             <Info></Info>
         </div>
     );
